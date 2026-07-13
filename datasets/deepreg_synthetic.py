6abe981170aa420ce3f473310be2ab8dd5c4bb1c
spellsharp <svcodin22@gmail.com>
2026-07-13 17:35:12 +0530

    Fix IndentationError in deepreg_synthetic sample generation

--- datasets/deepreg_synthetic.py
+++ datasets/deepreg_synthetic.py
@@ -218,24 +218,24 @@ class DeepRegLikeDVFSyntheticGenerator:
 
         moving_mask = None
         if fixed_mask is not None:
-           moving_mask = F.grid_sample(
-               fixed_mask.float(),
-               forward_grid,
-               mode="nearest",
-               padding_mode="border",
-               align_corners=False,
-        )
+            moving_mask = F.grid_sample(
+                fixed_mask.float(),
+                forward_grid,
+                mode="nearest",
+                padding_mode="border",
+                align_corners=False,
+            )
 
-       fixed_points = None
-       moving_points = None
+        fixed_points = None
+        moving_points = None
 
-       if fixed_mask is not None:
-           fixed_points = sample_foreground_points(base["fixed_mask"], num_points=8)
-           moving_points = warp_points_with_forward_grid(
-               fixed_points,
-               forward_dvf_grid,
-               self.image_size,
-           )
+        if fixed_mask is not None:
+            fixed_points = sample_foreground_points(base["fixed_mask"], num_points=8)
+            moving_points = warp_points_with_forward_grid(
+                fixed_points,
+                forward_dvf_grid,
+                self.image_size,
+            )
 
         # approximate inverse field for moving -> fixed
         gt_dvf_grid = -forward_dvf_grid
@@ -244,8 +244,8 @@ class DeepRegLikeDVFSyntheticGenerator:
         gt_dvf = normalized_xyz_to_monai_ddf(gt_dvf_grid, self.image_size)
 
         sample = {
-           "moving": moving[0],  # (1, D, H, W)
-           "fixed": fixed[0],    # (1, D, H, W)
+            "moving": moving[0],  # (1, D, H, W)
+            "fixed": fixed[0],    # (1, D, H, W)
             "dvf": gt_dvf,        # (3, D, H, W)
         }
 
